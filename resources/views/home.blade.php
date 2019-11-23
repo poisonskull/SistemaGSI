28760368c9cba65064179d99a9d0d8386b89d11a
MSK <rdevil4mu@gmail.com>
2019-11-23 15:58:28 -0600

    Se modifican vistas de activos y home, se carga bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,45 @@
 
 @section('content')
 <div class="container">
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">PROZA</h1><img src="/assets/logo.png">
+            <p class="lead">Hacer posible el uso de soluciones compartidas en tiempo y forma.
+            </p>
+        </div>
+    </div>
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Misión</h2>
+                <p>Mantener la confidencialidad, integridad y disponibilidad de la información que maneja PROZA, garantizando los niveles de seguridad que hoy en día son demandados para operar con un riesgo gestionado mediante la aplicación de controles de seguridad vanguardistas en todos los procesos y servicios que brinda PROZA.</p>
+                <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p> -->
+            </div>
+            <div class="col-md-4">
+                <h2>CONTACTOS DE EMERGENCIA</h2>
+                <b>ACTIVOS: </b> Luis Mario
+                Rodríguez Ruiz <a href="tel:+525529836702">(525529836702)</a><br>
+                <b>INFRAESTRUCTURA DE RED: </b> Luis Mario
+                Rodríguez Ruiz <a href="tel:+348773827488">(348773827488)</a><br>
+                <b>SEGURIDAD FÍSICA: </b> Luis Mario
+                Rodríguez Ruiz <a href="tel:+093298347482">(093298347482)</a><br>
+                <b>TI: </b> Luis Mario
+                Rodríguez Ruiz <a href="tel:+095498482923">(095498482923)</a><br>
+            </div>
+            <div class="col-md-4">
+                <h2>ESTATUS ACTUAL</h2>
+                <ul class="list-group">
+                        <li class="list-group-item"><b>Última revisión: </b> 01/Agosto/2019</li>
+                        <li class="list-group-item"><b>Último incidente: </b>06/octubre/2019</li>
+                        <li class="list-group-item"><b>Próxima revisión: </b>01/Febrero/2020</li>
+                        </li>
+                    </ul>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- <div class="col-md-6">
             <div class="card">
                 <div class="card-header">Lista de activos de TI</div>
                 <div class="card-body">                    
@@ -32,7 +69,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </div>
 @endsection
